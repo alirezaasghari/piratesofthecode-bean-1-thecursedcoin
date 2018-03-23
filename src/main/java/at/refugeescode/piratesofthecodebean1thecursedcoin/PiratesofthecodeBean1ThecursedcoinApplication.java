c06d92475f361777aa5f87168b80e9a91d14e2b0
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-03-23 19:33:27 +0100

    Add project structure

---
+++ src/main/java/at/refugeescode/piratesofthecodebean1thecursedcoin/PiratesofthecodeBean1ThecursedcoinApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.piratesofthecodebean1thecursedcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratesofthecodeBean1ThecursedcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratesofthecodeBean1ThecursedcoinApplication.class, args);
+	}
+}
