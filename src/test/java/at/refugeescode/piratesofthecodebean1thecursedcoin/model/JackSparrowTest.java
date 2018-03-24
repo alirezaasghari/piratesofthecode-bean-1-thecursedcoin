442fc4e1a63565a234bc0fd46bc8156d86f3ec2c
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-03-24 19:43:36 +0100

    Submitted Pirates of the code bean 1 - Test

---
+++ src/test/java/at/refugeescode/piratesofthecodebean1thecursedcoin/model/JackSparrowTest.java
@@ -0,0 +1,23 @@
+package at.refugeescode.piratesofthecodebean1thecursedcoin.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class JackSparrowTest {
+
+    @Autowired
+    private JackSparrow jackSparrow;
+
+    @Test
+    void findCoursedCoin() {
+        String message = jackSparrow.findCoursedCoin();
+        assertEquals("I got it!", message);
+    }
+}
